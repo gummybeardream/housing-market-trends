09b3cd70434ca6bde61fa6b112e571e141453350
Jessica Zou <jessicazou99@gmail.com>
2025-03-13 18:22:37 -0500

    Updated clean_homes_sold dataset with code adding a Country column and creating a csv for Tableau

--- scripts/clean_data/clean_homes_sold_df.py
+++ scripts/clean_data/clean_homes_sold_df.py
@@ -44,6 +44,10 @@ homes_sold_by_city_melted['Month'] = homes_sold_by_city_melted['Date'].dt.month
 #Extract year from Date column
 homes_sold_by_city_melted['Year'] = homes_sold_by_city_melted['Date'].dt.year
 
+#Add Country column with United States as the only value
+country_value = "United States"
+homes_sold_by_city_melted['Country'] = country_value
+
 #View dataset
 #print(homes_sold_by_city_melted.head(10))
 #print(homes_sold_by_city_melted.info())
@@ -52,16 +56,12 @@ homes_sold_by_city_melted['Year'] = homes_sold_by_city_melted['Date'].dt.year
 homes_sold_null = homes_sold_by_city_melted[homes_sold_by_city_melted.isnull().any(axis=1)]
 print(homes_sold_null)
 
-#Check on whether replacing 1191 null values with median/mean values will skew the data. compare both graphs
-#Convert HomesSold column values to int64. currently cannot convert because of NaN values
-
 #Plot number of HomesSold over 12 months to look at seasonal trends
-
 #Create dataframe without null values
 homes_sold_no_nulls = homes_sold_by_city_melted.dropna()
 
 #Check if nulls were dropped
-#print(homes_sold_no_nulls.info())
+print(homes_sold_no_nulls.info())
 
 #Create copy of the dataframe with Month and HomesSold columns. Drop the first row 
 homes_sold_barplot_columns = (homes_sold_no_nulls[['Month','HomesSold']].copy()).drop(index=0, inplace=False)
@@ -74,7 +74,15 @@ homes_sold_barplot_columns = (homes_sold_no_nulls[['Month','HomesSold']].copy())
 median_homes_sold_by_month = homes_sold_barplot_columns.groupby(['Month']).median().reset_index()
 total_homes_sold_by_month = homes_sold_barplot_columns.groupby(['Month']).sum().reset_index()
 
-print(median_homes_sold_by_month.head(12))
-print(total_homes_sold_by_month.head(12))
+#print(median_homes_sold_by_month.head(12))
+#print(total_homes_sold_by_month.head(12))
+
+#Create final dataframe to send to Tableau
+homes_sold_final = homes_sold_no_nulls[['Country','RegionID','StateName','City','HomesSold', 'Date', 'Month','Year']].drop(index=0, inplace=False)
+print(homes_sold_final.head())
+print(homes_sold_final.info())
+
+#Run to convert dataframe to csv file when dataset is ready for Tableau
+#homes_sold_final.to_csv("data/processed/homes_sold_final.csv")
 
 
