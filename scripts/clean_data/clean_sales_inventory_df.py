032ebd383030a4de33a5d9248b1473df8f1411de
Jessica Zou <jessicazou99@gmail.com>
2025-04-07 17:53:08 -0500

    Created sales_inventory_final CSV dataset to be sent to Tableau. Rearranged and clean up script for readability.

--- scripts/clean_data/clean_sales_inventory_df.py
+++ scripts/clean_data/clean_sales_inventory_df.py
@@ -33,7 +33,7 @@ sales_inventory_by_city.at[0, ('StateName', 'City')] = 'United States'
 sales_inventory_by_city_melted = pd.melt(
     sales_inventory_by_city, id_vars= ['RegionID', 'SizeRank', 'RegionName', 'RegionType', 'StateName', 'City'],
     var_name= 'Date',
-    value_name='HomesForSale'
+    value_name='ActiveListings'
 )
 
 #Convert Date column to datetime format 
@@ -42,8 +42,15 @@ sales_inventory_by_city_melted['Date'] = pd.to_datetime(sales_inventory_by_city_
 #Extract month from Date column
 sales_inventory_by_city_melted['Month'] = sales_inventory_by_city_melted['Date'].dt.month
 
+#Extract month name from Date column
+sales_inventory_by_city_melted['MonthName'] = sales_inventory_by_city_melted['Date'].dt.month_name()
+
 #Extract year from Date column
 sales_inventory_by_city_melted['Year'] = sales_inventory_by_city_melted['Date'].dt.year
+
+#Add Country column with United States as the only value
+country_value = 'United States'
+sales_inventory_by_city_melted['Country'] = country_value
 
 #View dataset
 #print(sales_inventory_by_city_melted.head(10))
@@ -51,38 +58,48 @@ sales_inventory_by_city_melted['Year'] = sales_inventory_by_city_melted['Date'].
 
 #Create a dataframe with null values
 sales_inventory_null = sales_inventory_by_city_melted[sales_inventory_by_city_melted.isnull().any(axis=1)]
-print(sales_inventory_null.info())
-print(sales_inventory_null.head(10))
+#print(sales_inventory_null.info())
+#print(sales_inventory_null.head(10))
 
-#Plot number of HomesForSale over 12 months to look at seasonal trends
 #Create dataframe without null values
 sales_inventory_no_nulls = sales_inventory_by_city_melted.dropna()
 
+#Check if nulls were dropped 
+#print(sales_inventory_no_nulls.info())
+
+#Convert ActiveListings values from float to whole numbers
+sales_inventory_no_nulls.loc[:,'ActiveListings'] = sales_inventory_no_nulls['ActiveListings'].round().astype(int)
+sales_inventory_no_nulls['ActiveListings'] = sales_inventory_no_nulls['ActiveListings'].astype(int)
+
+#Create final dataframe to send to Tableau. Drop the first row
+sales_inventory_final = sales_inventory_no_nulls[['Country', 'RegionID', 'StateName', 'City' , 'ActiveListings', 'Date', 'Month', 'MonthName', 'Year']].drop(index=0, inplace=False)
+#print(sales_inventory_final.head())
+
+#Run to convert dataframe to CSV file when dataset is ready for Tableau
+#sales_inventory_final.to_csv("data/processed/sales_inventory_final.csv")
+
+#Plot total number of ActiveListings over 12 months to look at seasonal trends
 #Create copy of the dataframe with only the Month and HomesForSale columns. Drop the first row 
-inventory_barplot_columns = (sales_inventory_no_nulls[['Month','HomesForSale']].copy()).drop(index=0, inplace=False)
+inventory_barplot_columns = (sales_inventory_no_nulls[['Month','ActiveListings']].copy()).drop(index=0, inplace=False)
 
 #Descriptive statistics for sales_inventory dataset with no nulls
-#print(inventory_barplot_columns.groupby('Month')['HomesForSale'].describe())
+#print(inventory_barplot_columns.groupby('Month')['ActiveListings'].describe())
 
-#Dateframe showing median or sum of HomesForSale grouped by month 
+#Dateframe showing median or sum of ActiveListings grouped by month 
 inventory_by_month_median = inventory_barplot_columns.groupby(['Month']).median().reset_index()
 inventory_by_month_sum = inventory_barplot_columns.groupby(['Month']).sum().reset_index()
 
 #print(inventory_by_month_sum.head(12))
 #print(inventory_by_month_median.head(12))
-
 #print(inventory_by_month.head(10))
 #print(inventory_by_month.info())
 #print(sales_inventory_no_nulls.head(10))
 #print(sales_inventory_no_nulls.info())
 
-#bar graph for HomesForSale trends over 12 months in a year
-'''plt.bar(x=inventory_by_month['Month'], height= inventory_by_month['HomesForSale'])
+#bar graph for ActiveListings trends over 12 months in a year
+'''plt.bar(x=inventory_by_month['Month'], height= inventory_by_month['ActiveListings'])
 plt.plot()
 plt.xlabel('Month number')
 plt.ylabel('Number of Homes Listed For Sale')
-plt.title('Best Month to Look at Home Listings')
+plt.title('Best Month to Look at Active Listings')
 plt.show()'''
-
-#Change numbers to more easily read millions. tableau demo. if you don't exit out of the plot, the terminal doesn't work when you run code
-#Plot the data when you put in 0s or median 
